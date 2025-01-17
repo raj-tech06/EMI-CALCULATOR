de62302c755438b8aa33dca1d449c6a3e76f7355
Abhishek Sahu <rjdeveloper07@gmail.com>
2025-01-17 17:41:00 +0530

    17th

--- calculator.js
+++ calculator.js
@@ -1,21 +1,21 @@
-// const display = document.getElementById('display');
+const display1 = document.getElementById('display');
 
-// function appendValue(value) {
-//     display.value += value;
-// }
+function appendValue(value) {
+    display.value += value;
+}
 
-// function clearDisplay() {
-//     display.value = '';
-// }
+function clearDisplay() {
+    display.value = '';
+}
 
-// function calculateResult() {
-//     try {
-//         display.value = eval(display.value);
-//     } catch (error) {
-//         alert('Invalid input');
-//         clearDisplay();
-//     }
-// }
+function calculateResult() {
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        alert('Invalid input');
+        clearDisplay();
+    }
+}
 
 
 
